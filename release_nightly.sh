00298c4c86e9682a377b58d81bef338e31195aad
Takuya Akiba <t.akiba.65536@gmail.com>
2023-07-30 22:08:55 +0900

    Add script for nightly release

---
+++ release_nightly.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -xeu
+
+# Create a temporary directory and clone the git repo
+readonly TEMPDIR=$(mktemp -d)
+
+# Trap to cleanup the TEMPDIR on exit
+trap "rm -rf $TEMPDIR" EXIT
+
+# Prepare the environment
+git clone https://github.com/iwiwi/epochraft.git ${TEMPDIR}
+python3 -m venv "${TEMPDIR}/venv"
+source "${TEMPDIR}/venv/bin/activate"
+cd "${TEMPDIR}"
+
+# Install twine
+pip install -U pip
+pip install -U twine wheel build
+
+# Check if the version ends with 'dev'
+readonly OLD_VERSION=$(grep '__version__' epochraft/version.py | cut -d '"' -f 2)
+if [[ "${OLD_VERSION}" != *dev ]]; then
+  echo "Version does not end with 'dev'. Aborting."
+  exit 1
+fi
+
+# Change the version file for scheduled TestPyPI upload
+readonly DATE=$(date +"%Y%m%d")
+readonly NEW_VERSION="${OLD_VERSION}${DATE}"
+sed -i.bak -e "s/${OLD_VERSION}/${NEW_VERSION}/" epochraft/version.py
+
+echo "Preparing to upload version: ${NEW_VERSION}"
+
+# Build a tar ball
+python -m build --sdist --wheel
+
+# Verify the distributions
+twine check dist/*
+
+# Publish distribution to TestPyPI
+twine upload -r testpypi dist/*
+
+# Ask for confirmation before uploading to PyPI
+read -r -p "Do you really want to upload to production PyPI? (y/N): " yn
+case $yn in
+  [Yy]* )
+    # Publish distribution to PyPI
+    twine upload -r pypi dist/* ;;
+  * )
+    echo "Aborting upload to production PyPI."; exit 1 ;;
+esac
+
+# Clean up
+deactivate
